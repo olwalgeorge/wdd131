73a77906e455536d9eda20557d7fca4cf30acb36
George Olwal <olwalgeorge@live.com>
2024-09-20 22:29:37 +0300

    js optimised

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,16 +1,29 @@
 /**
  * W01 Assignment: Home Page
- * gets the current year and formats the last modified date
+ * Gets the current year and formats the last modified date
  * @author George Olwal
  */
 
-const currentyear = document.querySelector("#currentyear");
-const lastModifiedElement = document.getElementById('lastModified');
-
-currentyear.textContent = new Date().getFullYear();
-
-const date = new Date();
-const dateOptions = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
-const usDateFormat = new Intl.DateTimeFormat('en-US', dateOptions).format(date);
-lastModifiedElement.textContent = usDateFormat;
-
+document.addEventListener('DOMContentLoaded', () => {
+    const currentYearElement = document.querySelector('#currentyear');
+    const lastModifiedElement = document.getElementById('lastModified');
+  
+    if (currentYearElement) {
+      currentYearElement.textContent = new Date().getFullYear();
+    }
+  
+    if (lastModifiedElement) {
+      const date = new Date();
+      const dateOptions = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+      };
+      const usDateFormat = new Intl.DateTimeFormat('en-US', dateOptions).format(date);
+      lastModifiedElement.textContent = usDateFormat;
+    }
+  });
