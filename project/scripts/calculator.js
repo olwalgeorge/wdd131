084f7cb0f5a5be9c280787b1d5ca16632558cbd1
George Olwal <olwalgeorge@live.com>
2024-10-30 18:00:24 +0300

    final

--- project/scripts/calculator.js
+++ project/scripts/calculator.js
@@ -384,3 +384,16 @@ function disableInputFields(disable) {
     document.getElementById('injectorRatio').disabled = disable;
     document.getElementById('stockTankCapacity').disabled = disable;
 }
+
+const images = document.querySelectorAll('img[data-src]');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      img.src = img.dataset.src;
+      observer.unobserve(img);
+    }
+  });
+});
+
+images.forEach(img => observer.observe(img));
--- project/scripts/project.js
+++ project/scripts/project.js
@@ -13,16 +13,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 });
 
-const images = document.querySelectorAll('img[data-src]');
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      const img = entry.target;
-      img.src = img.dataset.src;
-      observer.unobserve(img);
-    }
-  });
-});
 
-images.forEach(img => observer.observe(img));
 
