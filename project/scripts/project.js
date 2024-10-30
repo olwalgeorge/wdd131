b62835b3ed05868ef992746793b723d1dce1a06d
George Olwal <olwalgeorge@live.com>
2024-10-30 11:30:58 +0300

    lazy loading

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -13,3 +13,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 });
 
+const images = document.querySelectorAll('img[data-src]');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      img.src = img.dataset.src;
+      observer.unobserve(img);
+    }
+  });
+});
+
+images.forEach(img => observer.observe(img));
+
